dfa9ff0ec3dce1f1079dc641a2b1fda660de22c3
AbuRayhaan <91697183+AbuRayhaan@users.noreply.github.com>
2022-07-06 21:04:50 +0100

    Modified the schema.sql file

--- schema.sql
+++ schema.sql
@@ -1,5 +1,6 @@
 /* Database schema to keep the structure of entire database. */
 
+--CREATE TABLE FEATURE
 CREATE TABLE animals (
     id BIGSERIAL NOT NULL PRIMARY KEY,
     name varchar(100),
@@ -9,4 +10,12 @@ CREATE TABLE animals (
     weight_kg decimal
 );
 
+--QUERY AND UPDATE TABLE FEATURE
 ALTER TABLE animals ADD column species varchar(100);
+
+--QUERY MULTIPLE TABLES FEATURE
+CREATE TABLE owners (
+    id SERIAL PRIMARY KEY,
+    full_name varchar(100) NOT NULL,
+    age integer
+);
