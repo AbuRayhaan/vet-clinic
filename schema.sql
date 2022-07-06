006784f80c8e8fb7e38d443667b1669856ac63e3
AbuRayhaan <91697183+AbuRayhaan@users.noreply.github.com>
2022-07-06 22:47:52 +0100

    Created owners and species tables and altered the animals table

--- schema.sql
+++ schema.sql
@@ -19,3 +19,12 @@ CREATE TABLE owners (
     full_name varchar(100) NOT NULL,
     age integer
 );
+
+CREATE TABLE species (
+    id SERIAL PRIMARY KEY,
+    name varchar(100) NOT NULL
+);
+
+ALTER TABLE animals DROP column species;
+ALTER TABLE animals ADD column species_id int REFERENCES species(id);
+ALTER TABLE animals ADD column owner_id int REFERENCES owners(id);
